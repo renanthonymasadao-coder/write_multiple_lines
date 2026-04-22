ebd6f8bc3ac18e420cb65a6af8db5278d02abce4
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-04-22 22:26:48 +0800

    implement user input loop and file writing

--- file_writer.py
+++ file_writer.py
@@ -1,3 +1,13 @@
 class FileWriter:
     def __init__(self, file_name):
         self.file_name = file_name
+
+    def write_lines(self):
+        with open(self.file_name, 'w') as file:
+            while True:
+                line = input("Enter line: ")
+                file.write(line + "\n")
+
+                choice = input("Are there more lines? y/n: ").lower()
+                if choice == 'n':
+                    break
